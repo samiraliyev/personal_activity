cd75e64296c914db7635e068b1d17e83d012a932
samiraliyev <samir.aliyev@code.edu.az>
2024-04-05 03:15:37 +0400

    colorScheme turn off

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task/ui/home/home_page_view.dart';
 
-import 'constants/colors/app_color.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Task',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColor.badgeColor),
+        // colorScheme: ColorScheme.fromSeed(seedColor: AppColor.badgeColor),
         useMaterial3: true,
       ),
       home: const HomePageView(),
